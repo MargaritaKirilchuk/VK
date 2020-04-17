d9b39a9e6be94f6aa83ca41bf025e2312d3c2b06
Margarita <kirilchuk.margarita@gmail.com>
2020-04-17 23:33:15 +0300

    v1

---
+++ src/main/java/ru/netology/domain/Geo.java
@@ -0,0 +1,23 @@
+package ru.netology.domain;
+
+public class Geo {
+    private int coordinates;
+    private String place;
+
+    public int getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(int coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+}
---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,115 @@
+package ru.netology.domain;
+
+public class Post {
+    private int id;
+    private int date;
+    private Geo geo;
+    private String imageURL;
+    private String videoURL;
+    private String articlesURL;
+    private String text;
+    private LikesInfo likesInfo;
+    private CommentsInfo commentsInfo;
+    private RepostsInfo repostsInfo;
+    private ViewsInfo viewsInfo;
+    private String author;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public Geo getGeo() {
+        return geo;
+    }
+
+    public void setGeo(Geo geo) {
+        this.geo = geo;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public String getVideoURL() {
+        return videoURL;
+    }
+
+    public void setVideoURL(String videoURL) {
+        this.videoURL = videoURL;
+    }
+
+    public String getArticlesURL() {
+        return articlesURL;
+    }
+
+    public void setArticlesURL(String articlesURL) {
+        this.articlesURL = articlesURL;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public LikesInfo getLikesInfo() {
+        return likesInfo;
+    }
+
+    public void setLikesInfo(LikesInfo likesInfo) {
+        this.likesInfo = likesInfo;
+    }
+
+    public CommentsInfo getCommentsInfo() {
+        return commentsInfo;
+    }
+
+    public void setCommentsInfo(CommentsInfo commentsInfo) {
+        this.commentsInfo = commentsInfo;
+    }
+
+    public RepostsInfo getRepostsInfo() {
+        return repostsInfo;
+    }
+
+    public void setRepostsInfo(RepostsInfo repostsInfo) {
+        this.repostsInfo = repostsInfo;
+    }
+
+    public ViewsInfo getViewsInfo() {
+        return viewsInfo;
+    }
+
+    public void setViewsInfo(ViewsInfo viewsInfo) {
+        this.viewsInfo = viewsInfo;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+
+}
