98c70c09ff7cb6dc0832617d258302ac57124366
Margarita <kirilchuk.margarita@gmail.com>
2020-04-18 13:44:54 +0300

    PostManager added

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,16 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] post;
+
+    public Post [] search (int ownerId,String domain, String query, int count, int offset){
+        return null;
+    }
+
+    public void delete (int ownerId, int postId){
+        return;
+    }
+
+}
